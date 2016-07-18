8780740bfe3a02052de9f546fb5ca061237727ff
Jose Gonzalez <jose.gonzalez@webfg.com>
2016-07-18 16:48:47 +0200

    - Tagging Services (and having Fun!)

--- src/AppBundle/Twig/MarkdownExtension.php
+++ src/AppBundle/Twig/MarkdownExtension.php
@@ -3,8 +3,17 @@
 namespace AppBundle\Twig;
 
 
+use AppBundle\Service\MarkdownTransformer;
+
 class MarkdownExtension extends \Twig_Extension
 {
+    private $markdownTransformer;
+
+    public function __construct(MarkdownTransformer $markdownTransformer)
+    {
+        $this->markdownTransformer = $markdownTransformer;
+    }
+
     public function getFilters()
     {
         return [
@@ -14,7 +23,7 @@ class MarkdownExtension extends \Twig_Extension
 
     public function parseMarkdown($str)
     {
-        return strtoupper($str);
+        return $this->markdownTransformer->parse($str);
     }
 
     public function getName()
