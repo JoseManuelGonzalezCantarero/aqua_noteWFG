d3e3b3e5fec3e42574b31d48971cb5f8d0f29dbb
Jose Gonzalez <jose.gonzalez@webfg.com>
2016-07-20 16:53:47 +0200

    - The UserInterface Methods (Keep some Blank!)

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -2,33 +2,52 @@
 
 namespace AppBundle\Entity;
 
-
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
 class User implements UserInterface
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $email;
+
     public function getUsername()
     {
-        // TODO: Implement getUsername() method.
+        return $this->email;
     }
 
     public function getRoles()
     {
-        // TODO: Implement getRoles() method.
+        return ['ROLE_USER'];
     }
 
     public function getPassword()
     {
-        // TODO: Implement getPassword() method.
     }
 
     public function getSalt()
     {
-        // TODO: Implement getSalt() method.
     }
 
     public function eraseCredentials()
     {
-        // TODO: Implement eraseCredentials() method.
     }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
 }
